d301fedbe8f21e22f857ea2562c91039b016513c
Karar Al-Remahy <alremahykarar@ymail.com>
2021-05-17 00:13:15 +0200

    fix: change testing stuff

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ const github = require('@actions/github')
 const defaultSettings = {
   mwEndpoint: 'https://repair.wiki/api.php',
   userAgent: 'PPSL-Sync-Bot/%s (https://github.com/repair-manual/ppsl-sync)',
-  repoRemoteURL: 'https://github.com/kararty/ppsl-data.git',
+  repoRemoteURL: 'https://github.com/repair-manual/ppsl-data.git',
   repoLocalPath: path.join(__dirname, 'ppsl-data'),
   packageJSON: JSON.parse(fs.readFileSync(path.join(__dirname, './package.json'), 'utf-8')),
   appHomepage: 'https://repair-manual.github.io/ppsl-app'
